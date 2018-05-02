8db6274b45f9048082b542bb1cc85f708e7c7bae
Nicholas Stavrakakis <nicholas.stavrakakis@secretescapes.com>
2018-05-02 22:50:04 +0100

    fix incorrect ref to reaction type

--- src/components/Reaction/Reaction.jsx
+++ src/components/Reaction/Reaction.jsx
@@ -4,7 +4,7 @@ import React, { Component } from "react";
 import { observer } from "mobx-react";
 
 const Reaction = observer(({reaction}) => 
-    <div className={[styles.fadeOut, styles.reaction].join(' ')}>{reaction.name}</div>
+    <div className={[styles.fadeOut, styles.reaction].join(' ')}>{reaction.type}</div>
 );
 
 export default Reaction;
