2fe00d273ca599e2c38b8fe3302c38f3cbb77b82
Nicholas Stavrakakis <nicholas.stavrakakis@secretescapes.com>
2018-05-19 14:11:10 +0100

    add auth and calenadar look up to mobile app

--- src/mobileApp/index.jsx
+++ src/mobileApp/index.jsx
@@ -17,101 +17,15 @@ var config = {
   databaseURL: "https://meet-f3f8c.firebaseio.com",
   projectId: "meet-f3f8c",
   storageBucket: "",
-  messagingSenderId: "465362118588",
-
-  clientId:
-    "992715321827-7grtdl47sd1jp3jk7kqtlhbgj3ohi4l3.apps.googleusercontent.com",
-  scopes: ["email", "profile", "https://www.googleapis.com/auth/calendar"],
-  discoveryDocs: [
-    "https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"
-  ]
+  messagingSenderId: "465362118588"
 };
 
 firebase.initializeApp(config);
 
-var db = firebase.firestore();
-const settings = { timestampsInSnapshots: true };
-db.settings(settings);
-
-// window.addEventListener("beforeunload", function (e) {
-//   var confirmationMessage = "\o/";
-
-//   (e || window.event).returnValue = confirmationMessage; //Gecko + IE
-//   return confirmationMessage;                            //Webkit, Safari, Chrome
-// });
-
-function deleteUserSignals(email, roomId) {
-  let batch = db.batch();
-
-  return db
-    .collection("reactions")
-    .where("user", "==", email)
-    .where("room", "==", roomId)
-    .get()
-    .then(function(querySnapshot) {
-      querySnapshot.forEach(function(doc) {
-        batch.delete(doc.ref);
-      });
-    })
-    .then(() => {
-      batch.commit();
-    });
-}
-function setup(email, roomId, signalList) {
-  let i = 0;
-  signalList.signals = [];
-
-  // setup listeners on reaction list
-  db
-    .collection("reactions")
-    .where("room", "==", roomId)
-    .orderBy("timestamp", "desc")
-    .onSnapshot(function(querySnapshot) {
-      querySnapshot.docChanges.forEach(function(change) {
-        let doc = change.doc;
-        let d = change.doc.data();
-        if (change.type === "added") {
-          signalList.signals.push(
-            new UserSignal(doc.id, d.type, d.room, d.photoUrl, d.email)
-          );
-        }
-        if (change.type === "removed") {
-          var remove = signalList.signals.filter(r => {
-            return r.id === doc.id;
-          });
-          remove.forEach(r => {
-            signalList.signals.remove(r);
-          });
-        }
-      });
-    });
-}
-
-function signOut() {
-  var element = document.getElementById("meeting-app");
-  console.log("sign out and remove react from page");
-  localStorage.removeItem("currentMeetUser");
-  ReactDOM.unmountComponentAtNode(element);
-  element.remove();
-}
-
-function createContainer(id) {
-  var div = document.createElement("div");
-  div.id = id;
-  div.style.zIndex = "9999999";
-  return div;
-}
-
-function generateRandomId() {
-  return Math.random()
-    .toString(36)
-    .substring(4);
-}
-
 function render(rooms) {
   ReactDOM.render(
     <MobileAppContainer rooms={rooms} />,
-    document.getElementById(id)
+    document.getElementById("meeting-app")
   );
 }
 
@@ -120,7 +34,7 @@ function getCalendarEvents(accessToken, calendarId) {
     .startOf("day")
     .toISOString();
   const timeMax = moment()
-    .endOf("day")
+    .endOf("month")
     .toISOString();
 
   return fetch(
@@ -134,36 +48,12 @@ function getCalendarEvents(accessToken, calendarId) {
   )
     .then(res => res.json())
     .catch(error => console.error("Error:", error))
-    .then(response =>
-      response.items.filter(event => event.conferenceData).map(event => {
+    .then(response => {
+      return response.items.filter(event => event.conferenceData).map(event => {
         return { name: event.summary, id: event.conferenceData.conferenceId };
-      })
-    );
+      });
+    });
 }
-
-window.signOut = signOut;
-
-var id = "meeting-app";
-
-var container = createContainer(id);
-
-var signalList = new SignalList();
-var buttonList = new SignalButtonList();
-
-signalList.signals = [];
-
-buttonList.buttons.push(new SignalButton("1", "pan_tool", false));
-buttonList.buttons.push(new SignalButton("2", "thumb_up", false));
-buttonList.buttons.push(new SignalButton("3", "thumb_down", false));
-buttonList.buttons.push(new SignalButton("4", "access_time", false));
-
-let host = window.location.host;
-let room =
-  host === "meet.google.com"
-    ? window.location.pathname.substring(1)
-    : host === "hangouts.google.com"
-      ? window.location.pathname.split("/").reverse()[0]
-      : "general";
 
 var provider = new firebase.auth.GoogleAuthProvider();
 
@@ -177,100 +67,33 @@ firebase
     if (result && result.credential) {
       // This gives you a Google Access Token. You can use it to access the Google API.
       var token = result.credential.accessToken;
-
       var user = new User();
+
       user.email = result.user.email;
       user.photoUrl = result.user.photoURL;
       user.accessToken = result.credential.accessToken;
 
-      localStorage.setItem("currentMeetUser", JSON.stringify(user));
+      getCalendarEvents(user.accessToken, user.email).then(events =>
+        console.log(events)
+      );
 
-      getCalendarEvents(user.accessToken, user.email).then(rooms =>
-        render(rooms)
-      );
-    }
-  })
-  .catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // The email of the user's account used.
-    var email = error.email;
-    // The firebase.auth.AuthCredential type that was used.
-    var credential = error.credential;
-    // [START_EXCLUDE]
-    if (errorCode === "auth/account-exists-with-different-credential") {
-      alert(
-        "You have already signed up with a different auth provider for that email."
-      );
-      // If you are using multiple auth providers on your app you should handle linking
-      // the user's accounts here.
+      localStorage.setItem("currentMeetUser", JSON.stringify(user));
     } else {
-      console.error(error);
+      var provider = new firebase.auth.GoogleAuthProvider();
+
+      provider.addScope("email");
+      provider.addScope("https://www.googleapis.com/auth/calendar.readonly");
+
+      firebase.auth().signInWithRedirect(provider);
     }
-    // [END_EXCLUDE]
   });
 
 firebase.auth().onAuthStateChanged(function(userData) {
   if (userData) {
     // User is signed in.
+    console.log("signed in");
   } else {
     // User is signed out.
+    console.log("signed out");
   }
 });
-
-function toggleSignIn() {
-  if (!firebase.auth().currentUser) {
-    // [START createprovider]
-    var provider = new firebase.auth.GoogleAuthProvider();
-    // [END createprovider]
-    // [START addscopes]
-
-    provider.addScope("email");
-    provider.addScope("https://www.googleapis.com/auth/calendar.readonly");
-    // [END addscopes]
-    // [START signin]
-    firebase.auth().signInWithRedirect(provider);
-    // [END signin]
-  } else {
-    // [START signout]
-    firebase.auth().signOut();
-    // [END signout]
-  }
-  // [END_EXCLUDE]
-}
-
-window.toggleSignIn = toggleSignIn;
-
-// firebase
-//   .auth()
-//   .setPersistence(firebase.auth.Auth.Persistence.LOCAL)
-//   .then(() => {
-//     firebase
-//       .auth()
-//       .signInWithRedirect(provider);
-//   firebase.auth().getRedirectResult()
-//       .then(function(result) {
-//         var user = new User();
-//         user.email = result.user.email;
-//         user.photoUrl = result.user.photoURL;
-//         user.accessToken = result.credential.accessToken;
-
-//         localStorage.setItem("currentMeetUser", JSON.stringify(user));
-
-//         getCalendarEvents(user.accessToken, user.email).then(rooms =>
-//           render(rooms)
-//         );
-//       })
-//       .catch(function(error) {
-//         // Handle Errors here.
-//         console.log(error);
-//         var errorCode = error.code;
-//         var errorMessage = error.message;
-//         // The email of the user's account used.
-//         var email = error.email;
-//         // The firebase.auth.AuthCredential type that was used.
-//         var credential = error.credential;
-//         // ...
-//       });
-//   });
