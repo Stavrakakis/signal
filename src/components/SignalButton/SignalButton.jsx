97e125c53ce10d620f6915a8fe4de0bbf3203254
Nicholas Stavrakakis <nicholas.stavrakakis@secretescapes.com>
2019-04-13 19:58:15 +0100

    tidy up

--- src/components/SignalButton/SignalButton.jsx
+++ src/components/SignalButton/SignalButton.jsx
@@ -2,7 +2,6 @@ import styles from "./signalButton.css";
 import classNames from "classnames/bind";
 import React, { Component } from "react";
 
-import * as firebase from "firebase";
 import { observer } from "mobx-react";
 
 import animations from "animate.css";
@@ -18,7 +17,6 @@ class SignalButton extends Component {
       button.disabled = true;
       button.active = false;
 
-      console.log("removing " + button.reactionId);
       this.props.onRemoveSignal(button.reactionId);
     } else if (!button.disabled) {
       button.disabled = true;
@@ -31,50 +29,7 @@ class SignalButton extends Component {
       });
     }
   };
-  handleClick = e => {
-    var db = firebase.firestore();
-    let button = this.props.button;
-
-    if (button.active && !button.disabled) {
-      button.disabled = true;
-      button.active = false;
-
-      // var doc = db
-      //   .collection("reactions")
-      //   .doc(button.reactionId)
-      //   .delete()
-      //   .then(() => {
-      //     button.active = false;
-      //     button.disabled = false;
-      //   })
-      //   .catch(() => {
-      //     button.active = true;
-      //     button.disabled = false;
-      //   });
-    } else if (!button.disabled) {
-      // button.disabled = true;
-      // button.active = true;
-      // var doc = db
-      //   .collection("reactions")
-      //   .add({
-      //     room: this.props.room,
-      //     type: button.type,
-      //     user: this.props.user.email,
-      //     photoUrl: this.props.user.photoUrl,
-      //     timestamp: firebase.firestore.FieldValue.serverTimestamp()
-      //   })
-      //   .then(snap => {
-      //     button.active = true;
-      //     button.disabled = false;
-      //     button.reactionId = snap.id;
-      //   })
-      //   .catch(() => {
-      //     button.active = false;
-      //     button.disabled = false;
-      //   });
-    }
-  };
-
+  
   render() {
     let cx = classNames.bind(styles);
 
